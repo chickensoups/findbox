c15dd6060da2dfc290b6bb72339701d582ed00f8
Vuong <vuongkm01526@gmail.com>
2022-07-14 08:27:20 +0700

    Separate common files

---
+++ common.js
@@ -0,0 +1,15 @@
+let message = document.getElementById('message');
+
+function updateMessage(content) {
+  message.innerHTML = content;
+}
+
+
+function displaySuccessSound() {
+
+}
+
+function displayFailedSound() {
+
+}
+
