b754e5b5d318a6af8c09cec65c6bec82d51a31c1
Vuong <vuongkm01526@gmail.com>
2022-07-14 09:10:21 +0700

    Done level 0, level 1

--- common.js
+++ common.js
@@ -1,7 +1,30 @@
 let message = document.getElementById('message');
+let nextLevelBtn = document.getElementById('next-level-btn')
+const MSG_INFO = 'info'
+const MSG_SUCCESS = 'success'
+const MSG_FAIL = 'fail'
 
-function updateMessage(content) {
+function updateMessage(content, type = MSG_INFO) {
   message.innerHTML = content;
+  switch (type) {
+    case MSG_INFO:
+      message.style.color = 'black';
+      break;
+    case MSG_SUCCESS:
+      message.style.color = 'green';
+      break;
+    case MSG_FAIL:
+      message.style.color = 'red';
+      break;
+  }
+}
+
+function showTheNextLevelButton() {
+  nextLevelBtn.style.display = 'unset';
+}
+
+function nextLevel(levelName) {
+  window.location.href = levelName + '.html';
 }
 
 
